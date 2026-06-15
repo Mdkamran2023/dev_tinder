9ff898de4fd9079eaba0e96a177908248a08f4e0
Kamran <mdkamran0109@gmail.com>
2026-06-15 16:40:11 +0530

    exploring get,post,put

--- src/app.js
+++ src/app.js
@@ -3,13 +3,35 @@ const app= express(); //creating an instance of express application
 
 // request handling
 // app.get() is used to define a route handler for GET requests to the specified path
-app.get("/",(req,res)=>{
-    res.send("Hello World");
-});
+// here sequence matters, if we put the "/" route handler before the "/about" route handler,
+// then the "/about" route handler will never be reached because the "/" route handler will match all requests,including the "/about" request,
+//  and will send the response "Hello world" for all requests,including the "/about" request. 
+// Therefore, it's important to define the more specific route handlers before the more general ones to ensure that the correct handler is executed for each request.
 
 app.get("/about",(req,res)=>{
+    res.send("THis is the about get page")
+});
+
+app.post("/about",(req,res)=>{
+    res.send({firstname:"kammran",lastname:"shahzad"});
+});
+
+app.delete("/about",(req,res)=>{
+    res.send("This is the about delete page");
+});
+
+app.put("/about",(req,res)=>{
+    res.send("This is the about put page");
+}); 
+
+app.patch("/about",(req,res)=>{
+    res.send("This is the about patch page");
+});
+
+app.use("/about",(req,res)=>{
     res.send("This is the about page");
 });
+
 
 app.listen(7777,()=>{
     console.log("Server is running on port 7777 ...");
