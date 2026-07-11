e9e96e2020aa533c757520ce5c6ff8c3d22f0d89
Kamran <mdkamran0109@gmail.com>
2026-07-11 22:57:27 +0530

    token sent and receive for authentication

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,12 +1,27 @@
-const authMiddleware= (req,res,next)=>{
-    console.log("Admin middleware is running...");
-    const token="xyz@123"; //dummy token for authentication
-    const isAdminAuthorized= token ==="xyz@123"; //checking if the token is valid
-    if(isAdminAuthorized){
-        next(); //if authorized, proceed to the next middleware or route handler
-    } else{
-        res.status(403).send("Access denied. Admins only."); //if not authorized, send a 403 Forbidden response
+const jwt = require("jssonwebtoken");
+const {User} = require("../models/user");
+const userAuth=async(req,res,next)=>{
+    try{
+        //read the token from the cookies
+        const {token} = req.cookies;
+    //check if the user is authenticated
+    //if the token is not present, return an error
+    if(!token){
+        return res.status(401).json({message: "Unauthorized: No token provided .. Login to access this resource"});
     }
+    const decoded= await jwt.verify(token,"DEVMANUS@123");
+    //check if the user exists in the database
+    const user= await User.findById(decoded._id); 
+    if(!user){
+        throw new Error("User not found");
+    }
+    //if the user is authenticated, attach the user object to the request object
+    req.user=user;
+    next();
+
+}catch(err){
+    res.status(401).json({message: "Unauthorized: Invalid token"});
+}
 }
 
-module.exports={authMiddleware};
+module.exports={userAuth};
